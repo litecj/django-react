bfbd8e77c5de11b5088673b649b0bde74fcfb455
litecj <litecj702@gmail.com>
2021-10-05 14:25:29 +0900

    h1

--- ai-frontend/src/App.js
+++ ai-frontend/src/App.js
@@ -1,25 +1,22 @@
 import React from "react";
 import { Route,Switch, Redirect } from 'react-router-dom'
-import {Todo} from 'features/todos'
-import {SignUp} from 'features/user'
-import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
-// import { todoReducer, userReducer  } from 'reducers'
-
-import { CounterOld } from "features/counterOld";
-import { Linear, Mathematics, NonLinear } from "features/datastructure";
+import { Navi, Home } from "common";
 import { BackTracking, BruteForce, DivideConquer, DP, Greedy } from "features/algorithms";
-import { Home, Navi } from "common";
-// const rootReducer = combineReducers({todoReducer, userReducer})
-// const store = createStore(rootReducer)
+//import { Counter } from "features/counter";
+import { CounterOld } from "features/counterOld";
+import { Linear, NonLinear, Mathematics } from "features/datastructure";
+import { TodoInput, TodoList, Todo } from "features/todos";
+import { UserJoin, UserList, SignIn, SignUp } from "features/user";
+import { store } from 'app/store';
+
 const App = () => (
-    // <Provider store={store}>
-    <>
+    <Provider store={store}>
       <Navi/>
         <Switch>
           <Route exact path = '/' component = {Home}/>
           <Redirect from='/home' to= { '/' }/>
-          <Route exact path = '/counter' component = {CounterOld}/>
+          <Route exact path = '/counter-old' component = {CounterOld}/>
           <Route exact path = '/todo' component = {Todo}/>
           <Route exact path = '/sign-up' component = {SignUp}/>
           <Route exact path = '/mathematics' component = {Mathematics}/>
@@ -31,10 +28,10 @@ const App = () => (
           <Route exact path = '/dp' component = {DP}/>
           <Route exact path = '/greedy' component = {Greedy}/>
         </Switch>
-        </>
-    // </Provider>
+    </Provider>
 )
 export default App
+
 
 // import React from 'react';
 // import logo from './logo.svg';
--- ai-frontend/src/features/counter/components/Counter.js
+++ ai-frontend/src/features/counter/components/Counter.js
@@ -7,7 +7,7 @@ import {
   incrementAsync,
   incrementIfOdd,
   selectCount,
-} from '../counterSlice';
+} from 'features/counter/modules/counterSlice';
 import styles from './Counter.module.css';
 
 export function Counter() {
